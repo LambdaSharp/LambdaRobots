df7480cae77d2c5b0c128acc2dbb5dab7a46fff9
spencerk <spencerk@mindtouch.com>
2019-10-10 15:45:17 -0700

    robot updates

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -18,15 +18,16 @@ async function init() {
     5000,
     data => {
       gameBoardClient.Repaint(data);
+      updateRobotStats(data.Game.Robots);
       if (typeof data.Game.Messages !== "undefined") {
         messagesUi(data.Game.Messages);
-      }
-      if (data.Game.State === "Finished") {
-        stopGameUi();
       }
       if (data.Game.State === "Start") {
         sessionStorage.setItem("gameId", data.Game.Id);
         startGameUi();
+      }
+      if (data.Game.State === "Finished") {
+        stopGameUi();
       }
     }
   );
@@ -44,11 +45,7 @@ async function init() {
     localStorage.clear();
     window.location.href = "/";
   });
-  const robotArns = JSON.parse(localStorage.getItem("robotArns")) || [];
-  const robotArnsElements = [].slice.call(document.getElementsByName("robots"));
-  for (let index = 0; index < robotArns.length; index++) {
-    robotArnsElements[index].value = robotArns[index];
-  }
+  fillRobotArnsFromLocalStorage();
 }
 
 async function getConfig() {
@@ -62,7 +59,7 @@ async function getConfig() {
 }
 
 function startGame() {
-  const robotArns = getRobotArns();
+  const robotArns = getRobotArnsFromInputs();
   localStorage.setItem("robotArns", JSON.stringify(robotArns));
   const request = {
     Action: "start",
@@ -82,7 +79,15 @@ function stopGame() {
   wsClient.doSend(JSON.stringify(request));
 }
 
-function getRobotArns() {
+function fillRobotArnsFromLocalStorage() {
+  const robotArns = JSON.parse(localStorage.getItem("robotArns")) || [];
+  const robotArnsElements = [].slice.call(document.getElementsByName("robots"));
+  for (let index = 0; index < robotArns.length; index++) {
+    robotArnsElements[index].value = robotArns[index];
+  }
+}
+
+function getRobotArnsFromInputs() {
   const robotArnsElements = [].slice.call(document.getElementsByName("robots"));
   return robotArnsElements
     .map(robotArn => robotArn.value)
@@ -110,6 +115,45 @@ function messagesUi(messages) {
   messages.reverse().forEach(message => {
     messagesElement.appendChild(document.createTextNode(`${message.Text}\n`));
   });
+}
+
+function updateRobotStats(robots) {
+  const robotsStats = document.getElementById("robotsStats");
+  robotsStats.innerText = "";
+  for (let index = 0; index < robots.length; index++) {
+    const robot = robots[index];
+    const robotContainer = createElement("div");
+    robotContainer.appendChild(createElement("h3", `${robot.Name} (R${index}) (${robot.State})`));
+    const robotPrimaryStatsContainer = createElement("table");
+    const robotTr1 = createElement("tr");
+    robotTr1.appendChild(createElement("td", `Damage: ${robot.Damage}`));
+    robotTr1.appendChild(createElement("td", `Collision Damage: ${robot.CollisionDamage}`));
+    robotTr1.appendChild(createElement("td", `Total Damage Dealt: ${robot.TotalDamageDealt}`));
+    robotPrimaryStatsContainer.appendChild(robotTr1);
+    const robotTr2 = createElement("tr");
+    robotTr2.appendChild(createElement("td", `Missile Fire #: ${robot.TotalMissileFiredCount}`));
+    robotTr2.appendChild(createElement("td", `Missile Hit #: ${robot.TotalMissileHitCount}`));
+    robotTr2.appendChild(createElement("td", `Total Kills: ${robot.TotalKills}`));
+    robotPrimaryStatsContainer.appendChild(robotTr2);
+    const robotTr3 = createElement("tr");
+    robotTr3.appendChild(createElement("td", `X: ${Math.round(robot.X)}`));
+    robotTr3.appendChild(createElement("td", `Y: ${Math.round(robot.Y)}`));
+    robotTr3.appendChild(createElement("td", `Total Travel Distance: ${Math.round(robot.TotalTravelDistance)}`));
+    robotPrimaryStatsContainer.appendChild(robotTr3);
+    const robotPrimaryAllStats = createElement("pre");
+    robotPrimaryAllStats.innerText = JSON.stringify(robot, null, 2);
+    robotContainer.appendChild(robotPrimaryStatsContainer);
+    //robotContainer.appendChild(robotPrimaryAllStats);
+    robotsStats.appendChild(robotContainer);
+  }
+}
+
+function createElement(tag, text = "") {
+  const element = document.createElement(tag);
+  if (text.length > 0) {
+    element.appendChild(document.createTextNode(text));
+  }
+  return element;
 }
 
 window.addEventListener("load", init, false);
