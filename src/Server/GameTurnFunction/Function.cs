617c733e609597e27eef3406e2ea79c524217d3d
Steve Bjorg <steve.bjorg@gmail.com>
2021-10-06 14:22:40 -0700

    game loop is now in-place instead of recursive

--- src/Server/GameTurnFunction/Function.cs
+++ src/Server/GameTurnFunction/Function.cs
@@ -85,123 +85,96 @@ namespace LambdaRobots.Server.GameTurnFunction {
 
         public override async Task<FunctionResponse> ProcessMessageAsync(FunctionRequest request) {
 
-            // get game state from DynamoDB table
-            LogInfo($"Loading game state: ID = {request.GameId}");
-            var gameRecord = await _table.GetAsync<GameRecord>(request.GameId);
-            if(gameRecord == null) {
-
-                // game must have been stopped
+            // run game
+            try {
+                await GameLoopAsync(request.GameId);
+            } catch(Exception e) {
+                LogError(e, "error during game loop");
                 return new FunctionResponse {
                     GameId = request.GameId,
-                    Status = GameStatus.Finished
+                    Status = GameStatus.Error
                 };
             }
+            return new FunctionResponse {
+                GameId = request.GameId,
+                Status = GameStatus.Finished
+            };
+        }
 
-            // compute next turn
+        public async Task GameLoopAsync(string gameId) {
+
+            // get game state from DynamoDB table
+            LogInfo($"Loading game state: ID = {gameId}");
+            var gameRecord = await _table.GetAsync<GameRecord>(gameId);
+            if(gameRecord?.Game?.Status != GameStatus.Start) {
+
+                // TODO: log diagnostics
+                LogWarn($"Game state is not valid");
+                return;
+            }
             var game = gameRecord.Game;
-            LogInfo($"Game turn {game.TotalTurns}");
-            var logic = new GameLogic(new GameDependencyProvider(
-                gameRecord.Game,
-                _random,
-                robot => GetRobotBuildAsync(game, robot, gameRecord.LambdaRobotArns[gameRecord.Game.Robots.IndexOf(robot)]),
-                robot => GetRobotActionAsync(game, robot, gameRecord.LambdaRobotArns[gameRecord.Game.Robots.IndexOf(robot)])
-            ));
-
-            // determine game action to take
-            var messageCount = game.Messages.Count;
             try {
 
-                // check game state
-                switch(gameRecord.Game.Status) {
-                case GameStatus.Start:
+                // initialize game logic
+                var logic = new GameLogic(new GameDependencyProvider(
+                    gameRecord.Game,
+                    _random,
+                    robot => GetRobotBuildAsync(gameRecord.Game, robot, gameRecord.LambdaRobotArns[gameRecord.Game.Robots.IndexOf(robot)]),
+                    robot => GetRobotActionAsync(gameRecord.Game, robot, gameRecord.LambdaRobotArns[gameRecord.Game.Robots.IndexOf(robot)])
+                ));
 
-                    // start game
-                    LogInfo($"Start game: initializing {game.Robots.Count(robot => robot.Status == LambdaRobotStatus.Alive)} robots (total: {game.Robots.Count})");
-                    await logic.StartAsync(gameRecord.LambdaRobotArns.Count);
-                    game.Status = GameStatus.NextTurn;
-                    LogInfo($"Done: {game.Robots.Count(robot => robot.Status == LambdaRobotStatus.Alive)} robots ready");
-                    break;
-                case GameStatus.NextTurn:
+                // initialize robots
+                LogInfo($"Start game: initializing {game.Robots.Count(robot => robot.Status == LambdaRobotStatus.Alive)} robots (total: {game.Robots.Count})");
+                await logic.StartAsync(gameRecord.LambdaRobotArns.Count);
+                game.Status = GameStatus.NextTurn;
+                LogInfo($"Robots initialized: {game.Robots.Count(robot => robot.Status == LambdaRobotStatus.Alive)} robots ready");
+
+                // loop until we're done
+                var messageCount = game.Messages.Count;
+                while(game.Status == GameStatus.NextTurn) {
 
                     // next turn
                     LogInfo($"Start turn {game.TotalTurns} (max: {game.MaxTurns}): invoking {game.Robots.Count(robot => robot.Status == LambdaRobotStatus.Alive)} robots (total: {game.Robots.Count})");
                     await logic.NextTurnAsync();
                     LogInfo($"End turn: {game.Robots.Count(robot => robot.Status == LambdaRobotStatus.Alive)} robots alive");
-                    break;
-                case GameStatus.Finished:
 
-                    // nothing further to do
-                    break;
-                default:
-                    var status = game.Status;
-                    game.Status = GameStatus.Error;
-                    throw new ApplicationException($"unexpected game state: '{status}'");
-                }
-            } catch(Exception e) {
-                LogError(e, "error during game loop");
-                game.Status = GameStatus.Error;
-            }
+                    // log new game messages
+                    for(var i = messageCount; i < game.Messages.Count; ++i) {
+                        LogInfo($"Game message {i + 1}: {game.Messages[i].Text}");
+                    }
 
-            // log new game messages
-            for(var i = messageCount; i < game.Messages.Count; ++i) {
-                LogInfo($"Game message {i + 1}: {game.Messages[i].Text}");
-            }
-
-            // check if we need to update or delete the game from the game table
-            if(game.Status == GameStatus.NextTurn) {
-                LogInfo($"Storing game: ID = {game.Id}");
-
-                // attempt to update the game record
-                try {
+                    // attempt to update the game record
+                    LogInfo($"Storing game: ID = {gameId}");
                     await _table.UpdateAsync(new GameRecord {
-                        PK = game.Id,
+                        PK = gameId,
                         Game = game
                     }, new[] { nameof(GameRecord.Game) });
-                } catch {
-                    LogInfo($"Storing game failed: ID = {game.Id}");
 
-                    // the record failed to updated, because the game was stopped
-                    return new FunctionResponse {
-                        GameId = game.Id,
-                        Status = GameStatus.Finished
-                    };
+                    // notify WebSocket of new game state
+                    LogInfo($"Posting game update to connection: {gameId}");
+                    try {
+                        await _amaClient.PostToConnectionAsync(new PostToConnectionRequest {
+                            ConnectionId = gameRecord.ConnectionId,
+                            Data = new MemoryStream(Encoding.UTF8.GetBytes(LambdaSerializer.Serialize(new GameTurnNotification {
+                                Game = game
+                            })))
+                        });
+                    } catch(AmazonServiceException e) when(e.StatusCode == System.Net.HttpStatusCode.Gone) {
+
+                        // connection has been closed, stop the game
+                        LogInfo($"Connection is gone");
+                        game.Status = GameStatus.Finished;
+                        return;
+                    } catch(Exception e) {
+                        LogErrorAsWarning(e, "PostToConnectionAsync() failed");
+                    }
                 }
-            } else {
-                LogInfo($"Deleting game: ID = {game.Id}");
-                await _table.DeleteAsync<GameRecord>(game.Id);
-            }
+            } finally {
 
-            // notify WebSocket of new game state
-            LogInfo($"Posting game update to connection: {game.Id}");
-            try {
-                await _amaClient.PostToConnectionAsync(new PostToConnectionRequest {
-                    ConnectionId = gameRecord.ConnectionId,
-                    Data = new MemoryStream(Encoding.UTF8.GetBytes(LambdaSerializer.Serialize(new GameTurnNotification {
-                        Game = game
-                    })))
-                });
-            } catch(AmazonServiceException e) when(e.StatusCode == System.Net.HttpStatusCode.Gone) {
-
-                // connection has been closed, stop the game
-                LogInfo($"Connection is gone");
-                game.Status = GameStatus.Finished;
-                await _table.DeleteAsync<GameRecord>(game.Id);
-            } catch(Exception e) {
-                LogErrorAsWarning(e, "PostToConnectionAsync() failed");
+                // delete game from table
+                LogInfo($"Deleting game: ID = {gameId}");
+                await _table.DeleteAsync<GameRecord>(gameId);
             }
-
-            // check if we need to invoke the next game turn
-            if(game.Status == GameStatus.NextTurn) {
-                await _lambdaClient.InvokeAsync(new InvokeRequest {
-                    Payload = LambdaSerializer.Serialize(request),
-                    FunctionName = CurrentContext.FunctionName,
-                    InvocationType = InvocationType.Event
-                });
-            }
-            return new FunctionResponse {
-                GameId = game.Id,
-                Status = game.Status
-            };
         }
 
         private async Task<LambdaRobotBuild> GetRobotBuildAsync(Game game, LambdaRobot robot, string lambdaArn) {
