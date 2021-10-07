1d3d711d3892293a060fb15547b2766e4decfc3b
Steve Bjorg <steve.bjorg@gmail.com>
2021-10-07 11:48:45 -0700

    run tests before deploying

--- scripts/build-and-deploy.sh
+++ scripts/build-and-deploy.sh
@@ -3,6 +3,12 @@
 LAMBDAROBOTS_VERSION=2.0-DEV
 FOLDER=`pwd`
 
+dotnet test \
+    $FOLDER/Tests/Test.LambdaRobots.Server/Test.LambdaRobots.Server.csproj
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
 lash deploy \
     --module-version $LAMBDAROBOTS_VERSION \
     --allow-data-loss \
