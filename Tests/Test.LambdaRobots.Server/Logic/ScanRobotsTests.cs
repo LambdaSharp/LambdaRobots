8e8632533c7bac8ddb20d19acf1ce46d8b5bc212
Steve Bjorg <steve.bjorg@gmail.com>
2021-10-14 11:20:36 -0700

    remove duplicate unit tests

--- Tests/Test.LambdaRobots.Server/Logic/ScanRobotsTests.cs
+++ Tests/Test.LambdaRobots.Server/Logic/ScanRobotsTests.cs
@@ -22,13 +22,9 @@
  * SOFTWARE.
  */
 
-using System;
-using System.Collections.Generic;
-using LambdaRobots;
 using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
-using LambdaRobots.Bot.Model;
 
 namespace Test.LambdaRobots.Server {
 
@@ -42,121 +38,6 @@ namespace Test.LambdaRobots.Server {
 
         //--- Methods ---
 
-        #region *** Movement Tests ***
-        [Fact]
-        public void MoveRobotNorthForOneTurn() {
-
-            // arrange
-            var robot = NewRobot("Bob", 500, 500);
-            var logic = NewLogic(robot);
-            _robotActions["Bob"] = new List<Func<GetActionResponse>> {
-                () => new GetActionResponse {
-                    Heading = 0.0f,
-                    Speed = 100.0f
-                }
-            };
-
-            // act
-            logic.NextTurnAsync().Wait();
-
-            // assert
-            robot.X.Should().Be(500.0f);
-            robot.Y.Should().Be(505.0f);
-            robot.Speed.Should().Be(10.0f);
-        }
-
-        [Fact]
-        public void MoveRobotNorthForTwoTurns() {
-
-            // arrange
-            var robot = NewRobot("Bob", 500, 500);
-            var logic = NewLogic(robot);
-            _robotActions["Bob"] = new List<Func<GetActionResponse>> {
-                () => new GetActionResponse {
-                    Heading = 0.0f,
-                    Speed = 100.0f
-                }
-            };
-
-            // act
-            logic.NextTurnAsync().Wait();
-            logic.NextTurnAsync().Wait();
-
-            // assert
-            robot.X.Should().Be(500.0f);
-            robot.Y.Should().Be(520.0f);
-            robot.Speed.Should().Be(20.0f);
-        }
-
-        [Fact]
-        public void MoveRobotWestForOneTurn() {
-
-            // arrange
-            var robot = NewRobot("Bob", 500, 500);
-            var logic = NewLogic(robot);
-            _robotActions["Bob"] = new List<Func<GetActionResponse>> {
-                () => new GetActionResponse {
-                    Heading = 90.0f,
-                    Speed = 100.0f
-                }
-            };
-
-            // act
-            logic.NextTurnAsync().Wait();
-
-            // assert
-            robot.X.Should().Be(505.0f);
-            robot.Y.Should().Be(500.0f);
-            robot.Speed.Should().Be(10.0f);
-        }
-        #endregion
-
-        #region *** Collision Tests ***
-        [Fact]
-        public void MoveRobotCollideWithWall() {
-
-            // arrange
-            var robot = NewRobot("Bob", 990.0f, 990.0f);
-            robot.Heading = 30.0f;
-            robot.TargetHeading = robot.Heading;
-            robot.Speed = robot.MaxSpeed;
-            robot.TargetSpeed = robot.Speed;
-            var logic = NewLogic(robot);
-
-            // act
-            logic.NextTurnAsync().Wait();
-
-            // assert
-            robot.X.Should().BeLessThan(1000.0f);
-            robot.Y.Should().Be(1000.0f);
-            robot.Speed.Should().Be(0.0f);
-            robot.Damage.Should().Be(robot.CollisionDamage);
-            logic.Game.Messages.Count.Should().Be(2);
-            logic.Game.Messages[0].Text.Should().Be("Bob (R0) received 2 damage by wall collision");
-            logic.Game.Messages[1].Text.Should().Be("Bob is victorious! Game Over.");
-        }
-
-        [Fact]
-        public void MoveRobotColliedWithOtherRobot() {
-
-            // arrange
-            var bob = NewRobot("Bob", 500.0f, 500.0f);
-            var dave = NewRobot("Dave", 500.0f, 500.0f);
-            var logic = NewLogic(bob, dave);
-
-            // act
-            logic.NextTurnAsync().Wait();
-
-            // assert
-            bob.Damage.Should().Be(bob.CollisionDamage);
-            dave.Damage.Should().Be(dave.CollisionDamage);
-            logic.Game.Messages.Count.Should().Be(2);
-            logic.Game.Messages[0].Text.Should().Be("Bob (R0) was damaged 2 by collision with Dave (R1)");
-            logic.Game.Messages[1].Text.Should().Be("Dave (R1) was damaged 2 by collision with Bob (R0)");
-        }
-        #endregion
-
-        #region *** Scan Tests ***
         [Fact]
         public void ScanRobotInRange() {
 
@@ -202,25 +83,5 @@ namespace Test.LambdaRobots.Server {
             // assert
             robot.Should().Be(null);
         }
-        #endregion
-
-        #region *** Disqualification Tests ***
-        [Fact]
-        public void DisqualifiedDueToFailureToRespond() {
-
-            // arrange
-            var robot = NewRobot("Bob", 500, 500);
-            var logic = NewLogic(robot);
-            _robotActions["Bob"] = new List<Func<GetActionResponse>> {
-                () => null
-            };
-
-            // act
-            logic.NextTurnAsync().Wait();
-
-            // assert
-            robot.Status.Should().Be(BotStatus.Dead);
-        }
-        #endregion
     }
 }
